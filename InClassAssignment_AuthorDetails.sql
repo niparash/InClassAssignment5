35b9b52475cfe567e6c05ec85b019ad905889cf4
niparash <46506958+niparash@users.noreply.github.com>
2019-02-26 21:36:21 -0500

    SQL Files

---
+++ InClassAssignment_AuthorDetails.sql
@@ -0,0 +1,20 @@
+﻿USE [C:\USERS\PRIYANK\SOURCE\REPOS\INCLASSASSIGNMENT5\INCLASSASSIGNMENT5\APP_DATA\LIBRARY.MDF]
+GO
+
+/****** Object: Table [dbo].[AuthorDetails] Script Date: 26-02-2019 21:31:47 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[AuthorDetails]
+(
+	[Id] INT NOT NULL , 
+    [Author] VARCHAR(50) NOT NULL, 
+    [Country] VARCHAR(50) NULL, 
+    [Age] INT NULL, 
+    PRIMARY KEY ([Author])
+)
+
+
---
+++ InClassAssignment_Books.sql
@@ -0,0 +1,21 @@
+﻿USE [C:\USERS\PRIYANK\SOURCE\REPOS\INCLASSASSIGNMENT5\INCLASSASSIGNMENT5\APP_DATA\LIBRARY.MDF]
+GO
+
+/****** Object: Table [dbo].[Books] Script Date: 26-02-2019 21:33:20 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Books]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Book] NVARCHAR(200) NULL, 
+    [AuthorName] VARCHAR(50) NULL, 
+    [Publication] NVARCHAR(200) NULL, 
+    [Year] INT NULL
+    CONSTRAINT [FK_Books_ToAuthor] FOREIGN KEY ([AuthorName]) REFERENCES [AuthorDetails]([Author])
+)
+
+
